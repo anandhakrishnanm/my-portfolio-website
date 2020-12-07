bce9573bde4786044b38cf5b13144000b892f866
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2020-12-07 10:57:06 +0530

    bug fix

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 const menuBtn = document.getElementById('menuBtn');
 const navList = document.getElementById('nav-list');
 const links = document.querySelectorAll('.nav-link');
-console.log(links);
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     navList.classList.add('nav-items-active');
+    document.body.style.position = 'fixed';
+
     menuOpen = true;
   } else {
     menuBtn.classList.remove('open');
     navList.classList.remove('nav-items-active');
+    document.body.style.position = 'static';
     menuOpen = false;
   }
   
@@ -19,6 +21,7 @@ links.forEach(item => {
     item.addEventListener('click', () => {
         navList.classList.remove('nav-items-active');
         menuBtn.classList.remove('open');
+        document.body.style.position = 'static';
         menuOpen = false;
     });
         
